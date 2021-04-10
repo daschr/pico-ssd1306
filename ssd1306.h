b1f66c9734d4ac33a63661d82515122daf4974aa
David Schramm <david.schramm@live.com>
2021-04-10 21:15:41 +0200

    add missing declaration

--- ssd1306.h
+++ ssd1306.h
@@ -70,6 +70,7 @@ void ssd1306_clear(ssd1306_t *p);
 void ssd1306_draw_pixel(ssd1306_t *p, uint32_t x, uint32_t y);
 void ssd1306_draw_line(ssd1306_t *p, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
 void ssd1306_draw_square(ssd1306_t *p, uint32_t x, uint32_t y, uint32_t width, uint32_t height);
+void ssd13606_draw_empty_square(ssd1306_t *p, uint32_t x, uint32_t y, uint32_t width, uint32_t height);
 void ssd1306_draw_char_with_font(ssd1306_t *p, uint32_t x, uint32_t y, uint32_t scale, const uint8_t *font,  char c);
 void ssd1306_draw_char(ssd1306_t *p, uint32_t x, uint32_t y, uint32_t scale, char c);
 void ssd1306_draw_string_with_font(ssd1306_t *p, uint32_t x, uint32_t y, uint32_t scale, const uint8_t *font, char *s );
