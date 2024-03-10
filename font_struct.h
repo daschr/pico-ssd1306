792fec6ec9a34ff81280418b673c432b7563b0e4
Alexande Becker <nabla.becker@mailbox.org>
2024-03-10 20:32:56 +0800

    Define a struct for fonts
    Instead of encoding font properties in the bitmap buffer,
    Encode them in a struct, thus removing magic numbers in the code

---
+++ font_struct.h
@@ -0,0 +1,14 @@
+#ifndef _font_struct_h
+#define _font_struct_h
+#include <stdint.h>
+/**
+ * Struct that holds a font and all relevant information
+ */
+typedef struct {
+    uint16_t bytes_per_char; // the size of the char sprite in bytes (including padding)
+    uint16_t char_width; // width of the caracter in pixels (how many columns the sprite spans)
+    char first_char_in_font;
+    uint16_t char_height; // height of the character in pixels (how many rows the sprite spans)
+    const char *bitmap_buffer;
+} font;
+#endif
