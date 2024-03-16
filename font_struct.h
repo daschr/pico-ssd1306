4b1fa21157f7995a30eaff6629d8a0f70bf7ca79
Alexande Becker <nabla.becker@mailbox.org>
2024-03-16 18:19:43 +0800

    Commented the struct further

--- font_struct.h
+++ font_struct.h
@@ -7,7 +7,7 @@
 typedef struct {
     uint16_t bytes_per_char; // the size of the char sprite in bytes (including padding)
     uint16_t char_width; // width of the caracter in pixels (how many columns the sprite spans)
-    char first_char_in_font;
+    char first_char_in_font; // the first char that is included in the font
     uint16_t char_height; // height of the character in pixels (how many rows the sprite spans)
     const char *bitmap_buffer;
 } font;
